ffa00061cd246a4c1e7eec33ea81f9e54c98e3ca
Enzo Tiezzi <enzo_tiezzi@hotmail.com>
2021-10-31 18:04:45 -0300

    finish niece dialog

--- Source/DinoJAM/Level2InitialQuest.cpp
+++ Source/DinoJAM/Level2InitialQuest.cpp
@@ -3,14 +3,29 @@
 
 #include "Level2InitialQuest.h"
 
+#include "Kismet/GameplayStatics.h"
+
 void ULevel2InitialQuest::OnQuestStart(UWorld* World)
 {
 	Super::OnQuestStart(World);
 
 	OnStartingDialogFinish.BindUObject(this, &ULevel2InitialQuest::OnStartingDialogFinished);
+	OnDialogFinish.BindUObject(this, &ULevel2InitialQuest::OnStartingDialogFinished);
 }
 
 void ULevel2InitialQuest::OnStartingDialogFinished(UDialogItem* DialogItem)
 {
-	
+	UMyGameInstance* MyGameInstance = Cast<UMyGameInstance>(UGameplayStatics::GetGameInstance(DialogItem->World));
+
+    if(MyGameInstance)
+    {
+    	if(MyGameInstance->CurrentNPC)
+    	{
+    		MyGameInstance->CurrentNPC->GetMesh()->SetMaterial(MyGameInstance->CurrentNPC->FaceMaterialIndex, MyGameInstance->CurrentNPC->DefaultFaceExpression);
+    		MyGameInstance->CurrentNPC->StopAnimMontage();
+
+    		MyGameInstance->CurrentPlayerCharacter->GetMesh()->SetMaterial(MyGameInstance->CurrentPlayerCharacter->FaceMaterialIndex, MyGameInstance->CurrentPlayerCharacter->DefaultFaceExpression);
+    		MyGameInstance->CurrentPlayerCharacter->StopAnimMontage();
+    	}
+    }
 }
