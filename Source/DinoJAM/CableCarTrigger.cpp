0e910ac8ab91be4f1021abacffc84797d67e1540
Enzo Tiezzi <enzo_tiezzi@hotmail.com>
2021-11-04 09:16:40 -0300

    adding squence and cable car interaction

---
+++ Source/DinoJAM/CableCarTrigger.cpp
@@ -0,0 +1,84 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CableCarTrigger.h"
+
+#include "Gear.h"
+#include "InventorySystem.h"
+#include "LevelSequencePlayer.h"
+#include "MyGameInstance.h"
+#include "PlayerCharacter.h"
+#include "RedKey.h"
+#include "Components/SphereComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+// Sets default values
+ACableCarTrigger::ACableCarTrigger()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComponent"));
+
+	SphereComponent->SetupAttachment(GetRootComponent());
+}
+
+// Called when the game starts or when spawned
+void ACableCarTrigger::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACableCarTrigger::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ACableCarTrigger::Interact(APS1Character* Interactor)
+{
+	if(APlayerCharacter* Player = Cast<APlayerCharacter>(Interactor))
+	{
+		PlayerCharacter = Player;
+		
+		UMyGameInstance* MyGameInstance = Cast<UMyGameInstance>(UGameplayStatics::GetGameInstance(GetWorld()));
+
+		if(MyGameInstance)
+		{
+			AItem* RedKey = MyGameInstance->InventorySystem->GetItem(ARedKey::StaticClass());
+			AItem* Gear = MyGameInstance->InventorySystem->GetItem(AGear::StaticClass());
+
+			if(RedKey && Gear)
+			{
+				if(CableCarSequence)
+				{
+					FMovieSceneSequencePlaybackSettings MovieSceneSequencePlaybackSettings;
+					MovieSceneSequencePlaybackSettings.bHidePlayer = true;
+					MovieSceneSequencePlaybackSettings.bDisableMovementInput = true;
+					MovieSceneSequencePlaybackSettings.bHideHud = true;
+
+					ALevelSequenceActor* LevelSequenceActor;
+					
+					ULevelSequencePlayer* LevelSequencePlayer = ULevelSequencePlayer::CreateLevelSequencePlayer(GetWorld(), CableCarSequence, MovieSceneSequencePlaybackSettings, LevelSequenceActor);
+
+					if(LevelSequencePlayer)
+					{
+						LevelSequencePlayer->OnFinished.AddDynamic(this, &ACableCarTrigger::OnCableCarSequenceFinished);
+
+						LevelSequencePlayer->Play();
+					}
+				}
+			}
+		}
+	}
+}
+
+void ACableCarTrigger::OnCableCarSequenceFinished()
+{
+	if(CableCarExit)
+	{
+		PlayerCharacter->SetActorRelativeLocation(CableCarExit->GetActorLocation());
+	}
+}
---
+++ Source/DinoJAM/CableCarTrigger.h
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Interactable.h"
+#include "PlayerCharacter.h"
+#include "GameFramework/Actor.h"
+#include "CableCarTrigger.generated.h"
+
+UCLASS()
+class DINOJAM_API ACableCarTrigger : public AActor, public IInteractable
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ACableCarTrigger();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	class USphereComponent* SphereComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	class AActor* CableCarExit;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	class ULevelSequence* CableCarSequence;
+
+	UFUNCTION()
+	void OnCableCarSequenceFinished();
+
+	UPROPERTY(BlueprintReadWrite)
+	APlayerCharacter* PlayerCharacter;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void Interact(APS1Character* Interactor) override;
+};
