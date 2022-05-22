3869bc7be3b03b11875195bd6f578d836771af4b
Enzo Tiezzi <enzo_tiezzi@hotmail.com>
2022-05-22 18:31:04 -0300

    Revert "upgrading to unreal 5"
    This reverts commit 7d3a46682c307264a9408ebe3e0e7c7d3417cf7c.

--- Source/DinoJAM/DinoJAM.Build.cs
+++ Source/DinoJAM/DinoJAM.Build.cs
@@ -8,7 +8,7 @@ public class DinoJAM : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG", "LevelSequence", "MovieScene", "Landscape" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG", "LevelSequence", "MovieScene" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
